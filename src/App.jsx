ead20477cd2c0cf415a98789aec1e09e188f0be6
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-02-26 16:50:26 -0800

    Travis React

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './components/Todo.jsx'
-import Title from './components/Title.jsx'
-import Modal from './components/modal.jsx' 
+import Title from './components /Title.jsx'
+import Modal from './components/modal.jsx'
+import React, { useState } from 'react';
 
 
 function App() {
+ const [showModal, setShowModal] = useState(false)
+setShowModal(false)
   return (
     <div>
      
-  <h1>My Todo List</h1>
       <Title />
+      <div>
+        <input type="text" onChange={(event) => {
+            console.log(event.target.value)
+        }}/>
+        <button>Add todo</button>
+      </div>
 
       <div className="todo__wrapper">
-      <Todo 
-      title="Finish Frontend Simplified"
-      paragraph="Code along with fronend Simplified step by step."
-      />
-      <Todo 
-      title="Finish Interview section"
-      paragraph="Finish every interview question in the next 6 weeks."
-      />
-      <Todo 
-      title="Land a $100k job"
-      paragraph="Apply to 100 jobs."
-      />
+      <Todo title="Finish Frontend Simplified" />
+      <Todo title="Finish Interview section" />
+      <Todo title="Land a $100k job" />
     </div>
-   { /*<Modal title="Are you sure you want to delete?"/> 
-    <Modal title="Are you sure you want to add?"/> */}
+   {showModal && <Modal title="Confirm delete?" /> : null }
+    
     </div>
   );
 }
